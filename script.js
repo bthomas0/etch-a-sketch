8d9467c26ab47c2e8883ab63acdf62f511106778
bthomas0 <126216659+bthomas0@users.noreply.github.com>
2023-05-04 09:22:35 -0500

    Create for loops, fix typo

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ inner.id = 'inner';
 outer.appendChild(inner);
 
 function createGrid() {
+  // vertical
   for (let i = 0; i < userInput; i++) {
-    iterBox = document.createElement('div')
-    iterBox.id = `box-${i}`;
-    iterBox.textContent = i;
-    inner.appendChild(iterBox);
+    iBox = document.createElement('div');
+    iBox.className = `row`;
+    iBox.id = `row-${i}`;
+    iBox.textContent = i;
+    inner.appendChild(iBox);
+    for (let j = 0; j < userInput; j++) {
+      jBox = document.createElement('div')
+      jBox.id = `box-${j}`;
+      jBox.textContent = j;
+      iBox.appendChild(jBox);
+    }
   }
 }
 
