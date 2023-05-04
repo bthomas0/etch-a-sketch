faf9b191f9a91f394a70e3fac28d3551146f193a
bthomas0 <126216659+bthomas0@users.noreply.github.com>
2023-05-04 12:01:38 -0500

    Add border and center/center align the board

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 const outer = document.getElementById('outer');
 const inner = document.createElement('div');
-let userInput = 10;
+let userInput = 30;
+let totalArea = 600;
+let boxSize = totalArea / userInput;
 inner.id = 'inner';
 outer.appendChild(inner);
 
 function createGrid() {
   // vertical
   for (let i = 0; i < userInput; i++) {
-    iBox = document.createElement('div');
-    iBox.className = `row`;
-    iBox.id = `row-${i}`;
-    inner.appendChild(iBox);
+    row = document.createElement('div');
+    row.className = `row`;
+    row.id = `row-${i}`;
+    inner.appendChild(row);
+    // horizontal
     for (let j = 0; j < userInput; j++) {
-      jBox = document.createElement('div')
-      jBox.className = `box`;
-      jBox.id = `box-${i}-${j}`;
-      jBox.textContent = j;
-      iBox.appendChild(jBox);
+      box = document.createElement('div')
+      box.className = `box`;
+      box.id = `box-${i}-${j}`;
+      box.style.minHeight = `${boxSize}px`;
+      box.style.minWidth = `${boxSize}px`;
+      row.appendChild(box);
     }
   }
 }
