5965fe58b6001658bbc97f57b6a1d95d54115cf3
bthomas0 <126216659+bthomas0@users.noreply.github.com>
2023-05-04 14:09:34 -0500

    Add function for clearing the board

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-const outer = document.getElementById('outer');
-const inner = document.getElementById('inner');
+/*
+* TODO:
+* Add buttons (which could look like the knobs on the sides)?
+* Buttons: (1) User selects grid size | (2) Clear the board
+*
+*/ 
+
+
 let userInput = 30;
 let totalArea = 600;
 let boxSize = totalArea / userInput;
 
 function createGrid() {
+  const board = document.getElementById('board');
   // vertical (rows)
   for (let i = 0; i < userInput; i++) {
     row = document.createElement('div');
     row.className = `row`;
     row.id = `row-${i}`;
-    inner.appendChild(row);
+    board.appendChild(row);
     // horizontal (boxes)
     for (let j = 0; j < userInput; j++) {
       box = document.createElement('div')
@@ -21,10 +28,21 @@ function createGrid() {
       box.style.minWidth = `${boxSize}px`;
       row.appendChild(box);
       box.addEventListener('mouseover', (e) => {
-        console.log(e.target.classList.add('hovered'));
-      })
+        e.target.classList.add('hovered');
+      });
     }
   }
 }
 
+function clearBoard() {
+  const clearButton = document.getElementById('clear-btn');
+  clearButton.addEventListener('click', () => {
+    const hovered = document.getElementsByClassName('box');
+    for (let i = 0; i < hovered.length; i++) {
+      hovered[i].classList.remove('hovered');
+    }
+  });
+}
+
 createGrid();
+clearBoard();
