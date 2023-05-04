398dd22428d11a2977c38d344bb04dd447006822
bthomas0 <126216659+bthomas0@users.noreply.github.com>
2023-05-04 09:07:45 -0500

    Add starting function idea

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hello');
+const outer = document.getElementById('outer');
+const inner = document.createElement('div');
+let userInput = 10;
+inner.id = 'inner';
+outer.appendChild(inner);
+
+function createGrid() {
+  for (let i = 0; i < userInput; i++) {
+    iterBox = document.createElement('div')
+    iterBox.id = `box-${i}`;
+    iterBox.textContent = i;
+    inner.appendChild(iterBox);
+  }
+}
+
+createGrid();
