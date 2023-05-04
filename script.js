6f80c28557ea9554d1d99fee0c8d30d1febbc64f
bthomas0 <126216659+bthomas0@users.noreply.github.com>
2023-05-04 09:31:43 -0500

    Add flex to boxes

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ function createGrid() {
     iBox = document.createElement('div');
     iBox.className = `row`;
     iBox.id = `row-${i}`;
-    iBox.textContent = i;
     inner.appendChild(iBox);
     for (let j = 0; j < userInput; j++) {
       jBox = document.createElement('div')
-      jBox.id = `box-${j}`;
+      jBox.className = `box`;
+      jBox.id = `box-${i}-${j}`;
       jBox.textContent = j;
       iBox.appendChild(jBox);
     }
